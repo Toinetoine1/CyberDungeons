ac450cb8a2e81ab2d76adb0b4ddff8a6da0d55c9
Toinetoine1 <toinetoin1@gmail.com>
2021-02-23 14:01:53 +0100

    connecting to photon

---
+++ Assets/Scripts/Photon/TestConnect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Photon
+{
+    public class TestConnect : MonoBehaviourPunCallbacks
+    {
+        private void Start()
+        {
+            Debug.Log("Connecting to server...");
+            PhotonNetwork.GameVersion = "0.0.1";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Connected to server !");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.Log("Deconnecting: "+cause);
+        }
+    }
+}
