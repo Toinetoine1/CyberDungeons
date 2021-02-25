7c7c5ff5261ba0dce4a591d7f58e909f2fe15343
Toinetoine1 <toinetoin1@gmail.com>
2021-02-25 22:21:49 +0100

    ddd

--- Assets/Scripts/Photon/MainMenu/RoomListing.cs
+++ Assets/Scripts/Photon/MainMenu/RoomListing.cs
@@ -7,18 +7,20 @@ namespace Photon.MainMenu
 {
     public class RoomListing : MonoBehaviour
     {
+        [SerializeField] private CreateOrJoinRoomCanvas _createOrJoinRoomCanvas;
         [SerializeField] private Text text;
         public RoomInfo RoomInfo { get; private set;}
 
         public void SetRoomInfo(RoomInfo roomInfo)
         {
             RoomInfo = roomInfo;
-            text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
+            text.text = roomInfo.Name+ "("+roomInfo.PlayerCount+"/"+roomInfo.MaxPlayers+")";
         }
 
         public void onClickButton()
         {
             PhotonNetwork.JoinRoom(RoomInfo.Name);
+            _createOrJoinRoomCanvas.gameObject.SetActive(false);
         }
 
     }
