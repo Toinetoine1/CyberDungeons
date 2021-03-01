1f8f680dd86c338f71522a6bea1c2873de0fb7e7
Alexis Durfort <alexis.durfort@epita.fr>
2021-03-01 17:12:03 +0100

    began the healh system

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,36 @@
+﻿
+public class HealthSystem
+{
+    private int health;
+    public int Maxhealth;
+
+    public HealthSystem(int maxhealth)
+    {
+        Maxhealth = maxhealth;
+        health = maxhealth;
+    }
+
+    public int gethealth()
+    {
+        return health;
+    }
+
+    public float getHealthPercentage()
+    {
+        return (float) health / Maxhealth;
+    }
+
+    public void damage(int damageCount)
+    {
+        health -= damageCount;
+        if (health < 0)
+            health = 0;
+    }
+
+    public void heal (int healAmount)
+    {
+        health += healAmount;
+        if (health > Maxhealth)
+            health = Maxhealth;
+    }
+}
