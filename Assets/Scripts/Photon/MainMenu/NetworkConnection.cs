3e54c58239c66ffa581a32b8e6d644a13059402b
Alexis Durfort <alexis.durfort@epita.fr>
2021-03-02 22:52:04 +0100

    fixed animation sync with AI

--- Assets/Scripts/Photon/MainMenu/NetworkConnection.cs
+++ Assets/Scripts/Photon/MainMenu/NetworkConnection.cs
@@ -15,6 +15,7 @@ namespace Photon
         public void Connect()
         {
             Debug.Log("Connecting to server...");
+            PhotonNetwork.SendRate = 50;
             PhotonNetwork.AutomaticallySyncScene = true;
             PhotonNetwork.NickName = username.text;
             PhotonNetwork.GameVersion = gameVersion;
