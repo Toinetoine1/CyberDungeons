5412b30d8aa7ff3ecb4958aee9a121603ba62ff2
Toinetoine1 <toinetoin1@gmail.com>
2021-03-03 11:17:04 +0100

    fix player rotation

--- Assets/Scripts/AI/PlayerConnect.cs
+++ Assets/Scripts/AI/PlayerConnect.cs
@@ -22,41 +22,4 @@ public class PlayerConnect : MonoBehaviourPunCallbacks
             }   
         }
     }
-    
-    // void Start()
-    // {
-    //     PhotonNetwork.ConnectUsingSettings();
-    //     DefaultPool pool = PhotonNetwork.PrefabPool as DefaultPool;
-    //     pool.ResourceCache.Add(PlayerPrefab.name, PlayerPrefab);
-    //     
-    //     Debug.Log("1");
-    // }
-    //
-    // public override void OnConnectedToMaster()
-    // {
-    //     Debug.Log("2");
-    //     PhotonNetwork.JoinLobby();
-    // }
-    //
-    // public override void OnJoinedLobby()
-    // {
-    //     Debug.Log("3");
-    //     PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions() {MaxPlayers = 2}, TypedLobby.Default);
-    // }
-    //
-    // public override void OnJoinedRoom()
-    // {
-    //     GameObject obj = PhotonNetwork.Instantiate("Player", new Vector3(-4, 0, 0), Quaternion.identity);
-    //     obj.name = "1";
-    //     if (PhotonNetwork.IsMasterClient)
-    //     {
-    //         Debug.Log("set player1");
-    //         Player1 = obj;
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("set player2");
-    //         Player2 = obj;
-    //     }
-    // }
 }
