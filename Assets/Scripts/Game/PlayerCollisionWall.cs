7c8b5529a70516dd2fe9547e8bb63202d2b77b1a
leo guazzaroni <leo.guazzaroni@epita.fr>
2021-03-03 11:09:24 +0000

    petit rajout

---
+++ Assets/Scripts/Game/PlayerCollisionWall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerCollisionWall : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Detected");
+        FindObjectOfType<AudioManager>().Play("WallCollision");
+    }
+    
+}
