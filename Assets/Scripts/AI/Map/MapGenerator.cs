2f9d3f21ab5547dd81785f6e43ecbc299fb283b4
Toinetoine1 <toinetoin1@gmail.com>
2021-03-04 14:11:40 +0100

    change tilemap directory

---
+++ Assets/Scripts/AI/Map/MapGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using Random = System.Random;
+
+public class MapGenerator : MonoBehaviour
+{
+    public List<GameObject> availableMaps;
+
+    private Random _random = new Random();
+    
+    void Start()
+    {
+        //PhotonNetwork.Instantiate()   
+    }
+}
