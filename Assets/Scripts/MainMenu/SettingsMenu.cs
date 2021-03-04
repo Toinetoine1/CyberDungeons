3eeb47e4d40f97dc14ac42a1013d402937a04f12
leo guazzaroni <leo.guazzaroni@epita.fr>
2021-03-04 14:36:07 +0000

    rajout changement de résolution

--- Assets/Scripts/MainMenu/SettingsMenu.cs
+++ Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -16,12 +16,27 @@ public class SettingsMenu : MonoBehaviour
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
+        int currentResolutionsIndex = 0;
+        
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionsIndex = i;
+            }
         }
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionsIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height,Screen.fullScreen);
     }
 
     public void PlaySound()
