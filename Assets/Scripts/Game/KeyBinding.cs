28450e69ca0e31d5ad10fa2bf255ba2d34ed9132
Alexis Durfort <alexis.durfort@epita.fr>
2021-03-07 04:21:21 +0100

    Minor fixes on prefabs, added keybindings (should work lol)

---
+++ Assets/Scripts/Game/KeyBinding.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBinding : MonoBehaviour
+{
+    
+    [SerializeField] public List<string> Names;
+    [SerializeField] public List<KeyCode> KeyCodes;
+
+    private void Awake()
+    {
+        RetrieveBindings();
+    }
+
+    public void setKey(string key, KeyCode code)
+    {
+        if (Names.Contains(key))
+        {
+            int indexKey = Names.IndexOf(key);
+            KeyCodes[indexKey] = code;
+        }
+        else
+        {
+            Names.Add(key);
+            KeyCodes.Add(code);            
+        }
+    }
+
+    public void saveBindings()
+    {
+        PlayerPrefs.SetString("Key", JsonUtility.ToJson(Names));
+        PlayerPrefs.SetString("KeyCodes", JsonUtility.ToJson(KeyCodes));
+        PlayerPrefs.Save();
+    }
+
+    public void RetrieveBindings()
+    {
+        if (PlayerPrefs.HasKey("Key"))
+        {
+            Names.Clear();
+            KeyCodes.Clear();
+            string KeyJson = PlayerPrefs.GetString("Key");
+            string KeyCodesJson = PlayerPrefs.GetString("KeyCodes");
+            Names = JsonUtility.FromJson<List<string>>(KeyJson);
+            KeyCodes = JsonUtility.FromJson<List<KeyCode>>(KeyCodesJson);
+        }
+        else
+        {
+            setDefaultBind();
+        }
+    }
+
+    public void setDefaultBind()
+    {
+        setKey("UP", KeyCode.Z);
+        setKey("DOWN", KeyCode.S);
+        setKey("LEFT", KeyCode.Q);
+        setKey("RIGHT", KeyCode.D);
+        setKey("USE", KeyCode.E);
+    }
+
+    public int getKeyCodeIndex(string key)
+    {
+        return Names.IndexOf(key);
+    }
+}
---
+++ Assets/Scripts/MainMenu/KeyBindingsMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindingsMenu : MonoBehaviour
+{
+
+    private GameObject[] KeyBindButton;
+    private KeyBinding _keyBinding;
+
+    private string BindingName = string.Empty;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _keyBinding = GetComponent<KeyBinding>();
+        KeyBindButton = GameObject.FindGameObjectsWithTag("KeyBind");
+        foreach (var button in KeyBindButton)
+        {
+            string name = button.name;
+            Text tmp = button.GetComponentInChildren<Text>();
+            int index = _keyBinding.getKeyCodeIndex(name);
+            tmp.text = _keyBinding.KeyCodes[index].ToString();
+        }
+    }
+
+    public void onClickSet(string key)
+    {
+        BindingName = key;
+        Debug.Log(key);
+    }
+
+    private void SetText(string key, KeyCode code)
+    {
+        Text tmp = Array.Find(KeyBindButton, x => x.name == key).GetComponentInChildren<Text>();
+        tmp.text = code.ToString();
+    }
+
+    private void OnGUI()
+    {
+        if (BindingName != string.Empty)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                _keyBinding.setKey(BindingName, e.keyCode);
+                SetText(BindingName, e.keyCode);
+                BindingName = string.Empty;
+            }
+        }
+    }
+}
