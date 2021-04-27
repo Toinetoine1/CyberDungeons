2f797166aa1f1a8076618088f689e10d932f02e2
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2021-04-27 11:27:34 +0200

    Added Music from Level 1,2 and 3 with bosses music

---
+++ Assets/PlayerSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+    private float _timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer < 0)
+        {
+            _timer = 60f;
+            FindObjectOfType<AudioManager>().Play("PlayerHeroSound");
+        }
+    }
+}
