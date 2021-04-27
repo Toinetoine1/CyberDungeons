78039ccef6541c4befde6ec2be9f6bba1474d7f7
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2021-04-27 17:42:06 +0200

    rajout du spawning sur 1 prefab ( test)  !  PRB IA  A FIXER  !

---
+++ Assets/Scripts/Game/TriggerArea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun.UtilityScripts;
+using TMPro;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    public GameObject Prefab;
+    public Transform Spawner;
+    public bool haSpawned;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!haSpawned)
+        {
+            Instantiate(Prefab, Spawner.transform.position, Spawner.transform.rotation);
+            Debug.Log("TOUCHED");
+            haSpawned = true;
+        }
+    }
+}
