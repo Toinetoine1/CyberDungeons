dedbeabb9d3ffc32f5b230a32d9d75532114bd42
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2021-04-29 12:51:05 +0200

    Ajout sons + petit fix

---
+++ Assets/Scripts/Sound/TheLongShotAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheLongShotAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateSound", 4, 19);
+    }
+
+    // Update is called once per frame
+    void UpdateSound()
+    {
+        FindObjectOfType<AudioManager>().Play("TheLongShotAudio");
+    }
+}
