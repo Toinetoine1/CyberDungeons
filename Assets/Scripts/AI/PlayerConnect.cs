13cfab65b84c74ea8ecf45706b5c2fdc75c740aa
Toinetoine1 <toinetoin1@gmail.com>
2021-06-14 15:45:42 +0200

    add random enemy spawn

--- Assets/Scripts/AI/PlayerConnect.cs
+++ Assets/Scripts/AI/PlayerConnect.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
@@ -7,9 +8,12 @@ public class PlayerConnect : MonoBehaviourPunCallbacks
 {
     [SerializeField] 
     public GameObject PlayerPrefab;
+
+    public static List<GameObject> players;
  
     private void Start()
     {
+        players = new List<GameObject>();
         DefaultPool pool = PhotonNetwork.PrefabPool as DefaultPool;
         pool.ResourceCache.Add(PlayerPrefab.name, PlayerPrefab);
 
@@ -20,6 +24,7 @@ public class PlayerConnect : MonoBehaviourPunCallbacks
                 GameObject obj = PhotonNetwork.Instantiate("Player", new Vector3(-4, 0, 0), Quaternion.identity);
                 obj.name = pl.NickName;
                 obj.GetComponent<PhotonView>().TransferOwnership(pl);
+                players.Add(obj);
             }
         }
 
