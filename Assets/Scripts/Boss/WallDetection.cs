11535b539662440ceb3d82c447cf5d430c09aac0
Alexis Durfort <alexis.durfort@epita.fr>
2021-06-17 23:31:00 +0200

    petit ajout

---
+++ Assets/Scripts/Boss/WallDetection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetection : MonoBehaviour
+{
+    public RocketeerManagement RocketeerManagement;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        RocketeerManagement.Detected = true;
+    }
+}
