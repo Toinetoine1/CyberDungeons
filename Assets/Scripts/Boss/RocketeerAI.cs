83f48d6994710e725cdbaac525a337d0aadd52df
Alexis Durfort <alexis.durfort@epita.fr>
2021-06-17 23:30:09 +0200

    ajout de rocketeer (WIP)

--- Assets/Scripts/Boss/RocketeerAI.cs
+++ Assets/Scripts/Boss/RocketeerAI.cs
@@ -7,115 +7,156 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 
-
-public class RocketeerAI : MonoBehaviourPunCallbacks
+namespace AI
 {
-    protected GameObject target, pl1, pl2;
-
-    public float speed = 2f;
-    public float nextWaypointDistance = 1f;
-    public float lineOfSite = 5;
-    public float distToShot = 5;
-
-    private Path path;
-    private int currentWaypoint;
-
-    private Rigidbody2D rb;
-
-    protected Animator Animator;
-
-    private EnnemyWeapon ennemyWeapon;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class RocketeerAI : MonoBehaviourPunCallbacks
     {
-        rb = GetComponent<Rigidbody2D>();
-        Animator = GetComponent<Animator>();
-        ennemyWeapon = GetComponent<EnnemyWeapon>();
+        protected GameObject target, pl1, pl2;
 
-        StartCoroutine(ExecuteAfterTime(0.5f));
-        InvokeRepeating("UpdatePath", 0f, .5f);
-    }
+        public float speed = 2f;
+        public float nextWaypointDistance = 1f;
+        public float lineOfSite = 5;
+        public float distToShot = 5;
 
-    protected void SetMovementAnim(Vector2 dir)
-    {
-        Animator.SetBool("Standing", false);
-        Animator.SetFloat("xDir", dir.x);
-        Animator.SetFloat("yDir", dir.y);
-    }
+        private Path path;
+        private int currentWaypoint;
 
-    IEnumerator ExecuteAfterTime(float time)
-    {
-        yield return new WaitForSeconds(time);
+        private Seeker seeker;
+        private Rigidbody2D rb;
+        
+        private Animator Animator;
+        
+        private RocketeerManagement _rocketeerManagement;
 
-        foreach (Player pl in PhotonNetwork.CurrentRoom.Players.Values)
+
+        // Start is called before the first frame update
+        void Start()
         {
-            GameObject obj = GameObject.Find(pl.NickName);
-            if (pl1 == null)
-                pl1 = obj;
-            else
-                pl2 = obj;
+            //seeker = GetComponent<Seeker>();
+            rb = GetComponent<Rigidbody2D>();
+            Animator = GetComponent<Animator>();
+            _rocketeerManagement = GetComponent<RocketeerManagement>();
+
+            StartCoroutine(ExecuteAfterTime(0.5f));
+            InvokeRepeating("UpdatePath", 0f, .5f);
         }
-    }
-    
-
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        UpdateTarget(o =>
+        
+        protected void SetMovementAnim(Vector2 dir)
         {
-            if (path == null)
+            Animator.SetBool("Standing", false);
+            Animator.SetFloat("xDir", dir.x);
+            Animator.SetFloat("yDir", dir.y);
+        }
+        
+        IEnumerator ExecuteAfterTime(float time)
+        {
+            yield return new WaitForSeconds(time);
+
+            foreach (Player pl in PhotonNetwork.CurrentRoom.Players.Values)
+            {
+                GameObject obj = GameObject.Find(pl.NickName);
+                if (pl1 == null)
+                    pl1 = obj;
+                else
+                    pl2 = obj;
+            }
+        }
+
+        // private void UpdatePath()
+        // {
+        //     if (seeker.IsDone() && target != null)
+        //         seeker.StartPath(rb.position, target.transform.position, OnPathComplete);
+        // }
+
+        protected void OnPathComplete(Path p)
+        {
+            if (!p.error)
+            {
+                path = p;
+                currentWaypoint = 0;
+            }
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            UpdateTarget(o =>
+            {
+                if (path == null)
+                    return;
+                Debug.Log("Changing target");
+                seeker.CancelCurrentPathRequest();
+                seeker.StartPath(rb.position, target.transform.position, OnPathComplete);
+            });
+            
+            if (target == null)
                 return;
-            Debug.Log("Changing target");
-        });
-
-        if (path == null)
-            return;
-        if (target == null)
-            return;
-
-        ennemyWeapon.target = target.transform;
-    }
-
-    protected void UpdateTarget(Action<GameObject> callback)
-    {
-        GameObject oldTarget = target;
-
-        if (pl1 != null && pl2 == null)
-            target = pl1;
-        else if (pl2 != null && pl1 == null)
-            target = pl2;
-        else if (pl1 != null && pl2 != null)
-        {
-            var position = transform.position;
-            float distBetweenPl1AndEnemy = Vector2.Distance(position, pl1.transform.position);
-            float distBetweenPl2AndEnemy = Vector2.Distance(position, pl2.transform.position);
-
-            if (distBetweenPl2AndEnemy >= distBetweenPl1AndEnemy)
+            
+            _rocketeerManagement.target = target.transform;
+            
+            if (Vector2.Distance(transform.position, target.transform.position) <= distToShot &&
+                !Physics2D.Linecast(transform.position, target.transform.position, 1 << LayerMask.NameToLayer("WallColider")))
             {
+               Debug.Log("close");
+            }
+
+            // if (currentWaypoint >= 0 && currentWaypoint < path.vectorPath.Count)
+            // {
+            //
+            //     Vector2 nextPos = Vector2.MoveTowards(transform.position, path.vectorPath[currentWaypoint],
+            //         speed * Time.deltaTime);
+            //
+            //     SetMovementAnim((nextPos - (Vector2)transform.position).normalized);
+            //     transform.position = nextPos;
+            //     
+            //     float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+            //
+            //     if (distance < nextWaypointDistance)
+            //     {
+            //         currentWaypoint++;
+            //     }
+            // }
+        }
+
+        protected void UpdateTarget(Action<GameObject> callback)
+        {
+            GameObject oldTarget = target;
+            
+            if (pl1 != null && pl2 == null)
                 target = pl1;
-            }
-            else
-            {
+            else if (pl2 != null && pl1 == null)
                 target = pl2;
+            else if (pl1 != null && pl2 != null)
+            {
+                var position = transform.position;
+                float distBetweenPl1AndEnemy = Vector2.Distance(position, pl1.transform.position);
+                float distBetweenPl2AndEnemy = Vector2.Distance(position, pl2.transform.position);
+
+                if (distBetweenPl2AndEnemy >= distBetweenPl1AndEnemy)
+                {
+                    target = pl1;
+                }
+                else
+                {
+                    target = pl2;
+                }
+            }
+
+            if (oldTarget != target)
+            {
+                callback.Invoke(target);
             }
         }
 
-        if (oldTarget != target)
+
+        private void OnDrawGizmos()
         {
-            callback.Invoke(target);
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, lineOfSite);
+            
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(transform.position, distToShot);
         }
     }
 
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, lineOfSite);
-
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(transform.position, distToShot);
-    }
 }
--- Assets/Scripts/Boss/RocketeerManagement.cs
+++ Assets/Scripts/Boss/RocketeerManagement.cs
@@ -13,36 +13,65 @@ public class RocketeerManagement : MonoBehaviour
     public float dashInterval;
     private float currDashInterval;
 
+    public float speed;
+    private float currSpeed;
+
     private Rigidbody2D _rigidbody2D;
+
+    private bool isMoving;
+    public bool Detected;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         currfiringInterval = firingInTerval;
+        currDashInterval = dashInterval;
+        isMoving = false;
+        currSpeed = speed;
     }
 
     private void Update()
     {
+        Debug.Log(Detected);
         if (currfiringInterval > 0)
-        {
             currfiringInterval -= Time.deltaTime;
-        }
 
         if (currDashInterval > 0)
-        {
             currDashInterval -= Time.deltaTime;
+
+
+        
+        if (currDashInterval <= 0 && !Detected)
+            Move();
+        
+        if (isMoving)
+        {
+            currSpeed -= 0.25f;
+
+            if (Detected)
+                _rigidbody2D.velocity *= 0.75f;
+                
+            if (currSpeed == 0)
+            {
+                _rigidbody2D.velocity = Vector2.zero;
+                isMoving = false;
+                Detected = false;
+                currSpeed = speed;
+                currDashInterval = dashInterval;
+            }
         }
 
-        if (currDashInterval <= 0)
-        {
-            Move();
-        }
+        
     }
 
     private void Move()
     {
-        _rigidbody2D.velocity = target.position;
+        _rigidbody2D.velocity = (target.position - transform.position) * currSpeed; 
+        isMoving = true;
     }
-    
-    
+
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     Detected = true;
+    // }
 }
